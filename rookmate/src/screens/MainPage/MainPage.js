998e464ef6e38890b8ef5e663cb1c51deaffd3c2
Pua-Sangjun <daniellyu99@ailo.kr>
2023-06-23 00:10:13 +0900

    updated dev

---
+++ rookmate/src/screens/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import ReactDropdown from 'react-dropdown';
+import "../MainPage/MainPage.css"
+
+const MainPage = () => {
+const [button, setButton] = useState(false);
+
+console.log(button)
+  return (
+    <div id='mainPage'>
+      <h1>Welcome to My React App!</h1>
+      <p>This is the main page of my app.</p>
+      <button onClick={()=>setButton(true)}></button>
+    </div>
+  );
+};
+
+export default MainPage;
