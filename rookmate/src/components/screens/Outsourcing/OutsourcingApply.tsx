879ae351f34bc6d8c06a5b7a1bf023671ab737b6
Pua-Sangjun <daniellyu99@ailo.kr>
2023-06-25 03:09:56 +0900

    TopBar 사용방식 변경, prettier 적용

--- rookmate/src/components/screens/Outsourcing/OutsourcingApply.tsx
+++ rookmate/src/components/screens/Outsourcing/OutsourcingApply.tsx
@@ -1,37 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import TopBar from '../../TopBar';
-import './OutsourcingApply.css';
+import React, { useState, useEffect } from "react";
+import TopBar from "../../TopBar";
+import "./OutsourcingApply.css";
 import { images } from "../../../assets/images/images";
 
-const OutsourcingApply:React.FC = ()=>{
-  return(
+const OutsourcingApply: React.FC = () => {
+  return (
     <div>
-      <TopBar></TopBar>
-      <div className='outsourcingApply'>
-        <img src={images.leftPoint}/>
-        <div className='outsourcingInfo'>
-          <div className='infoBox'>
-            <p className='title'>시디학생의  로고디자인.타이포그래피</p>
-            <div className='contentHead'>
-              <img src={images.profile}/>
-              <div className='userInfo'>
-                <div className='name'>김용민</div>
-                <div className='organization'>국민대학교/소프트웨어학부</div>
+      <TopBar />
+      <div className="outsourcingApply">
+        <img src={images.leftPoint} />
+        <div className="outsourcingInfo">
+          <div className="infoBox">
+            <p className="title">시디학생의 로고디자인.타이포그래피</p>
+            <div className="contentHead">
+              <img src={images.profile} />
+              <div className="userInfo">
+                <div className="name">김용민</div>
+                <div className="organization">국민대학교/소프트웨어학부</div>
               </div>
-              <div className='others'>
-                <img src={images.mail}/>
-                <img src={images.mail}/>
-                <img src={images.mail}/>
-                <img src={images.mail}/>
+              <div className="others">
+                <img src={images.mail} />
+                <img src={images.mail} />
+                <img src={images.mail} />
+                <img src={images.mail} />
               </div>
             </div>
-            <div className='content'></div>
+            <div className="content"></div>
           </div>
-          <div className='imgBox'></div>
+          <div className="imgBox"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default OutsourcingApply;
