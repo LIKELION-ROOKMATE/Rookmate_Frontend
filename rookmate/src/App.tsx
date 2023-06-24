174420b09e96d55e53f4985f64b2e5ec4c9fb456
Pua-Sangjun <daniellyu99@ailo.kr>
2023-06-25 03:05:52 +0900

    필요없는 package.json 삭제

--- rookmate/src/App.tsx
+++ rookmate/src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/screens/MainPage/MainPage";
 import LoginPage from "./components/screens/LoginPage/LoginPage";
-import PortfolioEdit from './components/screens/PortfolioEditPage/PortfolioEdit';
-import OutsourcingApply from './components/screens/Outsourcing/OutsourcingApply';
+import PortfolioEdit from "./components/screens/PortfolioEditPage/PortfolioEdit";
+import OutsourcingApply from "./components/screens/Outsourcing/OutsourcingApply";
 
 const App: React.FC = () => {
   return (
@@ -12,12 +12,12 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/portfolio/edit" element={<PortfolioEdit />}/>
-          <Route path="/outsourcingApply" element={<OutsourcingApply />}/>
+          <Route path="/portfolio/edit" element={<PortfolioEdit />} />
+          <Route path="/outsourcingApply" element={<OutsourcingApply />} />
         </Routes>
       </Router>
     </div>
-	);
+  );
 };
 
 export default App;
