11402d621e70e555aab2ee236a37c44ca9c049ef
Pua-Sangjun <daniellyu99@ailo.kr>
2023-06-25 00:13:58 +0900

    changed all PortfolioStartPage to tsx

--- rookmate/src/App.tsx
+++ rookmate/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/screens/MainPage/MainPage";
 import LoginPage from "./components/screens/LoginPage/LoginPage";
+import PortfoliostartPage from "./components/screens/PortfolioStartPage/Portfoliostart";
+import PortfoliomakePage from "./components/screens/PortfoliomakePage/Portfoliomake";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +12,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/portfolio/start" element={<PortfoliostartPage />} />
+          <Route path="/portfolio/make" element={<PortfoliomakePage />} />
         </Routes>
       </Router>
     </div>
--- rookmate/src/assets/images/images.ts
+++ rookmate/src/assets/images/images.ts
@@ -4,6 +4,14 @@ import Logo from "../images/Logo.png";
 import MainPageTopAdImage from "../images/MainPageTopAdImage.png";
 import Profile from "../images/Profile.png";
 import Search from "../images/Search.png";
+import Ellipse3 from "../images/Ellipse3.png";
+import Ellipse4 from "../images/Ellipse4.png";
+import Ellipse5 from "../images/Ellipse5.png";
+import Image1 from "../images/image1.png";
+import Image2 from "../images/image2.png";
+import Image3 from "../images/image3.png";
+import MakeImage1 from "../images/makeimage1.png";
+import MakeImage2 from "../images/makeimage2.png";
 
 export const images = {
   deleteTag: DeleteTag,
@@ -12,4 +20,12 @@ export const images = {
   mainPageTopAdImage: MainPageTopAdImage,
   profile: Profile,
   search: Search,
+  ellipse3: Ellipse3,
+  ellipse4: Ellipse4,
+  ellipse5: Ellipse5,
+  image1: Image1,
+  image2: Image2,
+  image3: Image3,
+  MakeImage1: MakeImage1,
+  MakeImage2: MakeImage2,
 };
